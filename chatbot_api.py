394aea1922cb0663577f8301265dc0926eb3c54e
Stephenhng <97183468+Stephenhng@users.noreply.github.com>
2023-06-04 00:43:54 +0800

    Update chatbot_api.py

--- chatbot_api.py
+++ chatbot_api.py
@@ -10,7 +10,7 @@ import random
 import os
 from starlette.middleware.cors import CORSMiddleware
 from nltk.stem import WordNetLemmatizer
-from keras.models import load_model
+from model import TensorFlowModel
 from nltk.corpus import wordnet
 
 # localNLTK = "/nltk_data"
@@ -25,8 +25,6 @@ from nltk.corpus import wordnet
 #         nltk.data.path.append(localNLTK)
 nltk.download("punkt")
 nltk.download("wordnet")
-
-print(nltk.data.path)
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
@@ -56,21 +54,25 @@ app.add_middleware(
 class Massage(BaseModel):
     sentence: str
 
-        
-model = load_model('chatbot.h5')
+
+model = TensorFlowModel()
+model.load(os.path.join(os.getcwd(), 'model.tflite'))
 
 
 @app.get('/')
 def index():
     return {'message': 'This is the homepage of the API '}
 
+
 def is_good_word(sentence: str):
     wordnet.ensure_loaded()
+
 
 def clean_up_sentence(sentence: str):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
+
 
 def bag_of_words(sentence: str):
     sentence_words = clean_up_sentence(sentence)
@@ -80,6 +82,7 @@ def bag_of_words(sentence: str):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
 
 def predict_class(sentence: str):
     bow = bag_of_words(sentence)
@@ -92,6 +95,7 @@ def predict_class(sentence: str):
     for r in result:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
+
 
 def response(intents_list, intents_json, intents_json2, intents_json3):
     tag = intents_list[0]['intent']
@@ -112,6 +116,8 @@ def response(intents_list, intents_json, intents_json2, intents_json3):
                 elif k['tag'] == tag:
                     result = random.choice(k['responses'])
     return result
+
+
 print("Bot is running!")
 
 
