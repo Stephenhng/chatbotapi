301a02bedbdf0229772d3ed76bf50dc42787a068
Stephenhng <97183468+Stephenhng@users.noreply.github.com>
2023-06-04 00:04:54 +0800

    Update chatbot_api.py

--- chatbot_api.py
+++ chatbot_api.py
@@ -12,16 +12,20 @@ from starlette.middleware.cors import CORSMiddleware
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
-localNLTK = "/nltk_data"
-if os.path.isdir('/nltk_data'):
-    if localNLTK not in nltk.data.path:
-        nltk.data.path.append(localNLTK)
-else:
-    from zipfile import ZipFile
-    with ZipFile('nltk_data.zip', 'r') as zipObj:
-        zipObj.extractall(localNLTK)
-    if localNLTK not in nltk.data.path:
-        nltk.data.path.append(localNLTK)
+# localNLTK = "/nltk_data"
+# if os.path.isdir('/nltk_data'):
+#     if localNLTK not in nltk.data.path:
+#         nltk.data.path.append(localNLTK)
+# else:
+#     from zipfile import ZipFile
+#     with ZipFile('nltk_data.zip', 'r') as zipObj:
+#         zipObj.extractall(localNLTK)
+#     if localNLTK not in nltk.data.path:
+#         nltk.data.path.append(localNLTK)
+nltk.download("punkt")
+nltk.download("wordnet")
+
+print(nltk.data.path)
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
